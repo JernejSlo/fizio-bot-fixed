3d614de0f85a059110fe51cac30312ad97261c8a
JernejSlo <jernejkoprivnikar.slo@gmail.com>
2023-05-13 22:06:25 +0200

    Changed the design and added the start of the profile tab

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,23 @@
 import Header from "../Components/Header"
 import {AppWrapper} from "../Components/Constants"
+import {useState} from "react";
+import {sha256,login} from "../db"
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const user = await login(email, sha256(password));
+            console.log(user);
+            // redirect to dashboard or profile page
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
         <AppWrapper>
             <Header />
@@ -11,14 +27,14 @@ export default function Login() {
                     <form style={{ display: 'flex', flexDirection: 'column',alignItems: "center" }}>
                         <div style={{ marginBottom: '20px' }}>
                             <label htmlFor="email" style={{ display: 'block', marginBottom: '5px', color: '#555', fontWeight: 'bold' }}>Email Address</label>
-                            <input type="email" id="email" name="email" required placeholder="Enter your email address" style={{ padding: '10px', borderRadius: '5px', border: 'none', boxShadow: '0px 2px 5px rgba(0,0,0,0.1)' }} />
+                            <input onChange={e => setEmail(e.target.value)} type="email" id="email" name="email" required placeholder="Enter your email address" style={{ padding: '10px', borderRadius: '5px', border: 'none', boxShadow: '0px 2px 5px rgba(0,0,0,0.1)' }} />
                         </div>
                         <div style={{ marginBottom: '20px' }}>
                             <label htmlFor="password" style={{ display: 'block', marginBottom: '5px', color: '#555', fontWeight: 'bold' }}>Password</label>
-                            <input type="password" id="password" name="password" required placeholder="Enter your password" style={{ padding: '10px', borderRadius: '5px', border: 'none', boxShadow: '0px 2px 5px rgba(0,0,0,0.1)' }} />
+                            <input onChange={e => setPassword(e.target.value)} type="password" id="password" name="password" required placeholder="Enter your password" style={{ padding: '10px', borderRadius: '5px', border: 'none', boxShadow: '0px 2px 5px rgba(0,0,0,0.1)' }} />
                         </div>
                         <div style={{ display: 'flex', justifyContent: 'center',marginTop: "10px" }}>
-                            <button type="submit" style={{ backgroundColor: '#0077C9', color: '#fff', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Log in</button>
+                            <button onClick={e => handleSubmit()} type="submit" style={{ backgroundColor: '#0077C9', color: '#fff', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Log in</button>
                         </div>
                     </form>
                 </div>
--- src/db.js
+++ src/db.js
@@ -1,29 +1,25 @@
 const mysql = require('mysql2');
 
-const DB_HOST = "localhost"
-const DB_USER = "sis2022b"
-const DB_PASSWORD = "studenti"
-
 const conn = mysql.createConnection({
     host: "localhost",
-    user: "sis2022b",
-    password: "studenti",
-    database: 'Qcodeigniter',
+    user: "root",
+    password: "",
+    database: 'SISIII2023_89211282',
 })
-let dataPool={}
+let pool={}
 
-dataPool.allChats=(id)=>{
+pool.getAll=(id, table, val_to_match)=>{
     return new Promise ((resolve, reject)=>{
-        conn.query(`SELECT * FROM chats WHERE id = ?`, id, (err,res)=>{
+        conn.query(`SELECT * FROM ${table} WHERE ${val_to_match} = ?`, id, (err,res)=>{
             if(err){return reject(err)}
             return resolve(res)
         })
     })
 }
 
-dataPool.createChat=(title,slug,text)=>{
+pool.createChat=(title,slug,text)=>{
     return new Promise ((resolve, reject)=>{
-        conn.query(`INSERT INTO chats (title,slug,text) VALUES (?,?,?)`, [title, slug, text], (err,res)=>{
+        conn.query(`INSERT INTO pogovori (did,posiljatelj,vsebina) VALUES (?,?,?)`, [title, slug, text], (err,res)=>{
             if(err){return reject(err)}
             return resolve(res)
         })
@@ -37,3 +33,74 @@ conn.connect((err) => {
     }
     console.log('Connection established');
 })
+
+async function getData(func, id, table, to_match) {
+    try {
+        const result = await func(id, table, to_match);
+        return result;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+getData(pool.getAll, 0, "pogovori", "uid")
+    .then((result) => {
+        console.log(result);
+        const promises = result.map((row) => {
+            return getData(pool.getAll, row["Id"], "vsebinapogovorov", "cid");
+        });
+        return Promise.all(promises);
+    })
+    .then((results) => {
+        console.log(results);
+        conn.end();
+    })
+    .catch((error) => {
+        console.log(error);
+        conn.end();
+    });
+
+async function register(username, password) {
+    const conn = await pool.getConnection();
+    try {
+        await conn.beginTransaction();
+        const [rows, fields] = await conn.query('SELECT * FROM users WHERE username = ?', [username]);
+        if (rows.length > 0) {
+            throw new Error('Username already exists');
+        }
+        await conn.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password]);
+        await conn.commit();
+        return true;
+    } catch (err) {
+        await conn.rollback();
+        throw err;
+    } finally {
+        conn.release();
+    }
+}
+
+async function login(email, password) {
+    const conn = await pool.getConnection();
+    try {
+        const [rows, fields] = await conn.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password]);
+        if (rows.length === 0) {
+            throw new Error('Invalid username or password');
+        }
+        return rows[0];
+    } finally {
+        conn.release();
+    }
+}
+
+const sha256 = async (input) => {
+    const textAsBuffer = new TextEncoder().encode(input);
+    const hashBuffer = await window.crypto.subtle.digest("SHA-256", textAsBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hash = hashArray
+        .map((item) => item.toString(16).padStart(2, "0"))
+        .join("");
+    return hash;
+};
+
+export {register, login, sha256}
