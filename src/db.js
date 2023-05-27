1941efaa619f33efea80e03b472a47547cafe1b8
JernejSlo <jernejkoprivnikar.slo@gmail.com>
2023-05-27 12:32:20 +0200

    Updated database info to fit school server

--- src/db.js
+++ src/db.js
@@ -19,7 +19,6 @@ host: "localhost",
  */
 
 const conn = mysql.createConnection({
-    host: "SISIII2023_89211282",
     user: "codeigniter",
     password: "codeigniter2019",
     database: "SISIII2023_89211282",
